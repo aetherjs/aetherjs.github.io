3eed0ecc4a065bedfb3dc4ef3153b59dc0f235eb
Vlad Repinskiy <vladislav.repinskiy@zuehlke.com>
2023-07-10 19:18:44 +0200

    Add header in layout and fix routes issue

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,17 @@
 <script>
   import "../app.postcss";
-  export const prerender = true;
 </script>
 
-<slot />
+<header class="sticky top-0 z-50 bg-white py-2">
+  <nav class="flex justify-center">
+    <a href="/" class="mx-4 text-lg font-semibold text-gray-700">Home</a>
+    <a href="/honours" class="mx-4 text-lg font-semibold text-gray-700">Honours</a>
+    <a href="/notes" class="mx-4 text-lg font-semibold text-gray-700">Notes</a>
+    <a href="/lab" class="mx-4 text-lg font-semibold text-gray-700">Lab</a>
+  </nav>
+</header>
+
+
+<div class="pt-6 px-4">
+  <slot />
+</div>
