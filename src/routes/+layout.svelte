c7831e99eaddd9d334992eb1a469798efe86c044
Vlad Repinskiy <vladislav.repinskiy@zuehlke.com>
2023-07-12 14:14:06 +0200

    Add custom font, play with style a little bit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,11 +3,11 @@
 </script>
 
 <header class="sticky top-0 z-50 bg-white py-2">
-  <nav class="flex justify-center">
-    <a href="/" class="mx-4 text-lg font-semibold text-gray-700">Home</a>
-    <a href="/honours" class="mx-4 text-lg font-semibold text-gray-700">Honours</a>
-    <a href="/notes" class="mx-4 text-lg font-semibold text-gray-700">Notes</a>
-    <a href="/lab" class="mx-4 text-lg font-semibold text-gray-700">Lab</a>
+  <nav class="flex justify-center m-3">
+    <a href="/" class="mx-4 text-xl sm:text-3xl lg:text-5xl font-basteleur font-semibold ">Home</a>
+    <a href="/honours" class="mx-4 text-xl sm:text-3xl lg:text-5xl font-basteleur font-semibold">Honours</a>
+    <a href="/notes" class="mx-4 text-xl sm:text-3xl lg:text-5xl font-basteleur font-semibold">Notes</a>
+    <a href="/lab" class="mx-4 text-xl sm:text-3xl lg:text-5xl  font-basteleur font-semibold">Lab</a>
   </nav>
 </header>
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,11 @@ const config = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
 
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        basteleur: ['Basteleur', 'sans serif'],
+      },
+    },
   },
 
   plugins: [],
