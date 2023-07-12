a1f3535c8759349eda5e0ee4113ac93c9143f4f9
Vlad Repinskiy <vladislav.repinskiy@zuehlke.com>
2023-07-12 17:30:12 +0200

    Updates

--- _app/immutable/nodes/2.f18923aa.js
+++ _app/immutable/nodes/2.f18923aa.js
@@ -1,4 +1,4 @@
-import{s as i,n as a}from"../chunks/scheduler.e108d1fd.js";import{S as l,i as r,g as p,h as d,x as c,k as m,a as h,f}from"../chunks/index.10baf253.js";function u(o){let e,n=`<h1 class="text-2xl font-bold font-basteleur">Welcome, Stranger!</h1> <p>My name is Vlad Repinskiy, I am a Fullstack developer in the making.</p> <p>Even before university, I had experience with programming. Starting off with
+import{s as i,n as a}from"../chunks/scheduler.e108d1fd.js";import{S as l,i as r,g as p,h as d,x as c,k as m,a as h,f}from"../chunks/index.10baf253.js";function u(o){let e,n=`<h1 class="text-l sm:text-2xl lg:text-4xl font-bold font-basteleur">Welcome, Stranger!</h1> <p>My name is Vlad Repinskiy, I am a Fullstack developer in the making.</p> <p>Even before university, I had experience with programming. Starting off with
     competitive programming in school left me with a foundation to build upon in
     low-level languages like C and C++.</p> <p>Uni projects taught me Java, Kotlin (I really prefer the latter) and Python
     well. More specifically, I have good experience with OOP development,
@@ -13,4 +13,4 @@ import{s as i,n as a}from"../chunks/scheduler.e108d1fd.js";import{S as l,i as r,
     managed to work/cofound closely two DeFi projects. I am also really keen to
     capitalize on my understanding on architecture and low-level programming to
     add Rust to my skillset as soon as possible.</p> <p>I have plenty of experience in collaborative software development, and have
-    a soft spot for nice products, clean solutions and analytics. Let&#39;s work!</p>`;return{c(){e=p("div"),e.innerHTML=n,this.h()},l(t){e=d(t,"DIV",{class:!0,"data-svelte-h":!0}),c(e)!=="svelte-4p0h6s"&&(e.innerHTML=n),this.h()},h(){m(e,"class","grid grid-cols-1 gap-8 p-4 sm:p-8 lg:p-20")},m(t,s){h(t,e,s)},p:a,i:a,o:a,d(t){t&&f(e)}}}class y extends l{constructor(e){super(),r(this,e,null,u,i,{})}}export{y as component};
+    a soft spot for nice products, clean solutions and analytics. Let&#39;s work!</p>`;return{c(){e=p("div"),e.innerHTML=n,this.h()},l(t){e=d(t,"DIV",{class:!0,"data-svelte-h":!0}),c(e)!=="svelte-1kd90cb"&&(e.innerHTML=n),this.h()},h(){m(e,"class","grid grid-cols-1 gap-8 p-4 sm:p-8 lg:p-20")},m(t,s){h(t,e,s)},p:a,i:a,o:a,d(t){t&&f(e)}}}class y extends l{constructor(e){super(),r(this,e,null,u,i,{})}}export{y as component};
