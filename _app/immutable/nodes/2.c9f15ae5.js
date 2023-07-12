61847f7254041e026d7b3f47b52d9ea6617bdc54
Vlad Repinskiy <vladislav.repinskiy@zuehlke.com>
2023-07-12 17:52:43 +0200

    Updates

---
+++ _app/immutable/nodes/2.c9f15ae5.js
@@ -0,0 +1,15 @@
+import{s as F,n as C}from"../chunks/scheduler.e108d1fd.js";import{S as M,i as q,g as m,s as u,h as d,x as c,c as f,k as D,a as n,f as l}from"../chunks/index.10baf253.js";function J(S){let a,k="Welcome, Stranger!",v,s,g="My name is Vlad Repinskiy, I am a Fullstack developer in the making.",h,o,b=`Even before university, I had experience with programming. Starting off with
+  competitive programming in school left me with a foundation to build upon in
+  low-level languages like C and C++.`,x,i,I=`Uni projects taught me Java, Kotlin (I really prefer the latter) and Python
+  well. More specifically, I have good experience with OOP development, various
+  build/CI/CD systems, I understand JVM well and I can write good code. I have
+  used python in diverse environments and use cases, for standalone apps and
+  scripts, for making simple web APIs, and most importantly I worked quite a lot
+  with python notebooks, for data analysis, mining and retrieval tasks.
+  Frameworks like pandas, numpy, tensorflow, stuff like that.`,y,r,_=`For some time, I been working on the basic skillset necessary for DeFi
+  development: teaching myself Solidity & EVM, along with JavaScript with all
+  its blockchain dev toolset: hardhat, truffle, foundry, cast. As a result, I
+  managed to work/cofound closely two DeFi projects. I am also really keen to
+  capitalize on my understanding on architecture and low-level programming to
+  add Rust to my skillset as soon as possible.`,w,p,P=`I have plenty of experience in collaborative software development, and have a
+  soft spot for nice products, clean solutions and analytics. Let's work!`;return{c(){a=m("h1"),a.textContent=k,v=u(),s=m("p"),s.textContent=g,h=u(),o=m("p"),o.textContent=b,x=u(),i=m("p"),i.textContent=I,y=u(),r=m("p"),r.textContent=_,w=u(),p=m("p"),p.textContent=P,this.h()},l(t){a=d(t,"H1",{class:!0,"data-svelte-h":!0}),c(a)!=="svelte-1nuxh9h"&&(a.textContent=k),v=f(t),s=d(t,"P",{"data-svelte-h":!0}),c(s)!=="svelte-17ylys"&&(s.textContent=g),h=f(t),o=d(t,"P",{"data-svelte-h":!0}),c(o)!=="svelte-183bw8t"&&(o.textContent=b),x=f(t),i=d(t,"P",{"data-svelte-h":!0}),c(i)!=="svelte-10nq0wx"&&(i.textContent=I),y=f(t),r=d(t,"P",{"data-svelte-h":!0}),c(r)!=="svelte-1tkmcyr"&&(r.textContent=_),w=f(t),p=d(t,"P",{"data-svelte-h":!0}),c(p)!=="svelte-bsg29v"&&(p.textContent=P),this.h()},h(){D(a,"class","text-l sm:text-2xl lg:text-4xl font-bold font-basteleur")},m(t,e){n(t,a,e),n(t,v,e),n(t,s,e),n(t,h,e),n(t,o,e),n(t,x,e),n(t,i,e),n(t,y,e),n(t,r,e),n(t,w,e),n(t,p,e)},p:C,i:C,o:C,d(t){t&&(l(a),l(v),l(s),l(h),l(o),l(x),l(i),l(y),l(r),l(w),l(p))}}}class A extends M{constructor(a){super(),q(this,a,null,J,F,{})}}export{A as component};
