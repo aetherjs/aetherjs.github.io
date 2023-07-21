a8f1d6da97ed9d5c7f045f3f43a45e6aadbaf5cb
Vlad Repinskiy <vladislav.repinskiy@zuehlke.com>
2023-07-21 15:21:10 +0200

    Updates

---
+++ _app/immutable/nodes/2.d028d621.js
@@ -0,0 +1,48 @@
+import{s as R,n as N}from"../chunks/scheduler.140875e2.js";import{S as H,i as K,g as s,s as o,h as l,z as a,c as r,k as W,a as i,f as n}from"../chunks/index.a2e716c4.js";function B(Q){let u,F="Welcome, Stranger!",h,c,V="My name is Vlad Repinskiy, I am a Fullstack developer in the making.",g,m,A=`Even before university, I had experience with programming. Starting off with
+  competitive programming in school left me with a foundation to build upon in
+  low-level languages like C and C++.`,y,p,M=`Uni projects taught me Java, Kotlin (I really prefer the latter) and Python
+  well. More specifically, I have good experience with OOP development, various
+  build/CI/CD systems, I understand JVM well and I can write good code. I have
+  used python in diverse environments and use cases, for standalone apps and
+  scripts, for making simple web APIs, and most importantly I worked quite a lot
+  with python notebooks, for data analysis, mining and retrieval tasks.
+  Frameworks like pandas, numpy, tensorflow, stuff like that.`,k,d,z=`For some time, I been working on the basic skillset necessary for DeFi
+  development: teaching myself Solidity & EVM, along with JavaScript with all
+  its blockchain dev toolset: hardhat, truffle, foundry, cast. As a result, I
+  managed to work/cofound closely two DeFi projects. I am also really keen to
+  capitalize on my understanding on architecture and low-level programming to
+  add Rust to my skillset as soon as possible.`,w,v,E=`I have plenty of experience in collaborative software development, and have a
+  soft spot for nice products, clean solutions and analytics. Let's work!`,I,f,J=`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sed nisl
+  semper, fermentum turpis eget, aliquam velit. Nunc quis ligula nisl. Curabitur
+  eu pharetra urna, ut dapibus elit. Sed turpis tortor, tristique vel dignissim
+  ac, convallis eget lacus. Suspendisse cursus urna quis libero consequat, eu
+  eleifend lectus congue. Sed efficitur dolor porttitor nunc pharetra, vitae
+  ullamcorper lacus iaculis. Curabitur euismod auctor porttitor. Aliquam eget
+  aliquam lacus, a lacinia mauris. Nullam quis blandit tortor. Phasellus sit
+  amet nulla et lectus dignissim bibendum sit amet in nunc. Nulla sit amet arcu
+  viverra, egestas orci vitae, maximus nisi.`,P,x,U=`Nullam eu consectetur sapien, euismod porta tortor. Pellentesque condimentum
+  vel nunc non efficitur. Nunc sit amet dolor congue, sodales metus quis,
+  scelerisque orci. Nam tristique dapibus orci vitae molestie. Nulla mollis non
+  ex non consectetur. Donec at eleifend ante, et pellentesque elit. In fringilla
+  lectus ac nibh consectetur rhoncus. Duis lobortis id eros et posuere. Integer
+  finibus erat metus. Etiam pharetra, leo ac pulvinar tristique, turpis enim
+  lobortis diam, at porttitor ex odio vitae dolor.`,_,C,j=`Suspendisse ultricies varius orci non viverra. Donec egestas cursus libero,
+  vitae accumsan eros posuere eu. Ut enim ipsum, tempor at lacus pellentesque,
+  luctus venenatis nisi. Suspendisse quis porta massa, at feugiat nulla.
+  Vestibulum ultricies lectus quis pellentesque gravida. Cras malesuada sit amet
+  est ornare cursus. Duis a erat nec purus rutrum congue. Fusce tempus sodales
+  tortor. Nulla sit amet lectus dolor. Duis porttitor massa a dictum lobortis.`,S,b,L=`Vivamus neque quam, finibus eu ornare nec, tincidunt non orci. Nunc ultrices
+  dui id velit maximus, a dictum neque finibus. Donec convallis sodales lacinia.
+  Aenean tristique convallis tincidunt. Nunc dapibus magna dolor, vitae cursus
+  dui posuere non. In mattis lectus ex. Quisque quis placerat dolor. In luctus
+  metus vel dolor iaculis condimentum. Vivamus lobortis interdum tortor, a
+  ultricies augue. Sed porttitor facilisis tortor, non sagittis ligula lacinia
+  at. Ut id imperdiet lorem. In pharetra nisi at suscipit pulvinar. Pellentesque
+  sit amet consectetur nisi. Donec tristique magna eu enim rutrum pretium. Class
+  aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos
+  himenaeos.`,D,q,O=`Cras sed purus vel mauris mollis congue id vel dui. Quisque ipsum nisl,
+  scelerisque a nibh vitae, pellentesque suscipit felis. Suspendisse facilisis
+  elit id enim ullamcorper, ut elementum nisi pretium. Morbi aliquet facilisis
+  arcu. Pellentesque habitant morbi tristique senectus et netus et malesuada
+  fames ac turpis egestas. Donec auctor volutpat arcu id ullamcorper. Nulla
+  faucibus ligula a sapien volutpat, vitae viverra neque fringilla.`;return{c(){u=s("h1"),u.textContent=F,h=o(),c=s("p"),c.textContent=V,g=o(),m=s("p"),m.textContent=A,y=o(),p=s("p"),p.textContent=M,k=o(),d=s("p"),d.textContent=z,w=o(),v=s("p"),v.textContent=E,I=o(),f=s("p"),f.textContent=J,P=o(),x=s("p"),x.textContent=U,_=o(),C=s("p"),C.textContent=j,S=o(),b=s("p"),b.textContent=L,D=o(),q=s("p"),q.textContent=O,this.h()},l(e){u=l(e,"H1",{class:!0,"data-svelte-h":!0}),a(u)!=="svelte-15md4cp"&&(u.textContent=F),h=r(e),c=l(e,"P",{"data-svelte-h":!0}),a(c)!=="svelte-17ylys"&&(c.textContent=V),g=r(e),m=l(e,"P",{"data-svelte-h":!0}),a(m)!=="svelte-183bw8t"&&(m.textContent=A),y=r(e),p=l(e,"P",{"data-svelte-h":!0}),a(p)!=="svelte-10nq0wx"&&(p.textContent=M),k=r(e),d=l(e,"P",{"data-svelte-h":!0}),a(d)!=="svelte-1tkmcyr"&&(d.textContent=z),w=r(e),v=l(e,"P",{"data-svelte-h":!0}),a(v)!=="svelte-bsg29v"&&(v.textContent=E),I=r(e),f=l(e,"P",{"data-svelte-h":!0}),a(f)!=="svelte-17aa72k"&&(f.textContent=J),P=r(e),x=l(e,"P",{"data-svelte-h":!0}),a(x)!=="svelte-1eu8y20"&&(x.textContent=U),_=r(e),C=l(e,"P",{"data-svelte-h":!0}),a(C)!=="svelte-15qbkez"&&(C.textContent=j),S=r(e),b=l(e,"P",{"data-svelte-h":!0}),a(b)!=="svelte-1m5i5vy"&&(b.textContent=L),D=r(e),q=l(e,"P",{"data-svelte-h":!0}),a(q)!=="svelte-5ykzur"&&(q.textContent=O),this.h()},h(){W(u,"class","text-l sm:text-2xl lg:text-4xl font-bold font-karla")},m(e,t){i(e,u,t),i(e,h,t),i(e,c,t),i(e,g,t),i(e,m,t),i(e,y,t),i(e,p,t),i(e,k,t),i(e,d,t),i(e,w,t),i(e,v,t),i(e,I,t),i(e,f,t),i(e,P,t),i(e,x,t),i(e,_,t),i(e,C,t),i(e,S,t),i(e,b,t),i(e,D,t),i(e,q,t)},p:N,i:N,o:N,d(e){e&&(n(u),n(h),n(c),n(g),n(m),n(y),n(p),n(k),n(d),n(w),n(v),n(I),n(f),n(P),n(x),n(_),n(C),n(S),n(b),n(D),n(q))}}}class X extends H{constructor(u){super(),K(this,u,null,B,R,{})}}export{X as component};
