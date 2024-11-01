3c5bed81598ce1694e5aef22cabcbcc01e647ac4
Vlad Repinskiy <vladislav.repinskiy@zuehlke.com>
2024-11-01 19:02:40 +0100

    Add some logging to the wishlist to debug in prod

--- src/routes/wishlist/+page.server.ts
+++ src/routes/wishlist/+page.server.ts
@@ -1,4 +1,5 @@
 import { Client } from "@notionhq/client";
+
 import { NOTION_API_KEY, NOTION_DATABASE_ID } from "$lib/env";
 import type { WishlistItem } from "./types";
 
@@ -7,26 +8,36 @@ const notion = new Client({
 });
 
 export async function load() {
-  const response = await notion.databases.query({
-    database_id: NOTION_DATABASE_ID,
-    sorts: [
-      {
-        property: "Category",
-        direction: "ascending",
-      },
-    ],
-  });
+  console.log("Starting Notion API call");
 
-  const items: WishlistItem[] = response.results.map((page) => {
-    const props = page.properties;
-    return {
-      name: props.Name.title[0]?.plain_text || "",
-      link: props.Link?.url || "",
-      price: props.Price?.number,
-      category: props.Category?.select?.name || "Uncategorized",
-      description: props.Description?.rich_text[0]?.plain_text || "",
-    };
-  });
+  try {
+    const response = await notion.databases.query({
+      database_id: NOTION_DATABASE_ID,
+      sorts: [
+        {
+          property: "Category",
+          direction: "ascending",
+        },
+      ],
+    });
 
-  return { items };
+    console.log("API call successful");
+    console.log("Number of items:", response.results.length);
+
+    const items: WishlistItem[] = response.results.map((page) => {
+      const props = page.properties;
+      return {
+        name: props.Name.title[0]?.plain_text || "",
+        link: props.Link?.url || "",
+        price: props.Price?.number,
+        category: props.Category?.select?.name || "Uncategorized",
+        description: props.Description?.rich_text[0]?.plain_text || "",
+      };
+    });
+
+    return { items };
+  } catch (error) {
+    console.error("Notion API Error:", error);
+    return { items: [] };
+  }
 }
