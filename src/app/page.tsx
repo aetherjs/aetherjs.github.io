6c8803df40058f5e5096dc9341ebf655a42c15f7
Vlad Repinskiy <vladislav.repinskiy@zuehlke.com>
2025-01-18 21:23:03 +0100

    Build a WIP component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,5 @@
+import { WorkInProgress } from "@/components/WorkInProgress";
+
 export default function Home() {
-  return (
-    <div className='min-h-screen flex flex-col items-center justify-center p-8'>
-      <h1 className='font-heading text-6xl font-bold mb-8'>Work in Progress</h1>
-      <p className='font-body text-lg max-w-2xl text-center'>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur.
-      </p>
-    </div>
-  );
+  return <WorkInProgress />;
 }
---
+++ src/components/WorkInProgress.tsx
@@ -0,0 +1,26 @@
+export const WorkInProgress = () => {
+  return (
+    <div className='min-h-screen flex flex-col items-center justify-center p-8'>
+      <h1 className='font-heading text-6xl font-bold mb-4'>Work in Progress</h1>
+      <picture className='mb-8'>
+        <source
+          srcSet='https://fonts.gstatic.com/s/e/notoemoji/latest/1f6a7/512.webp'
+          type='image/webp'
+        />
+        <img
+          src='https://fonts.gstatic.com/s/e/notoemoji/latest/1f6a7/512.gif'
+          alt='🚧'
+          width='64'
+          height='64'
+        />
+      </picture>
+      <p className='font-body text-lg max-w-2xl text-center'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur.
+      </p>
+    </div>
+  );
+};
