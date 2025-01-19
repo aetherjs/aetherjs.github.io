861c39b49aea76472c1061ad8eb26b7a3cf9d045
Vlad Repinskiy <vladislav.repinskiy@zuehlke.com>
2025-01-19 20:37:26 +0100

    Style the toogle a little more

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,6 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useTheme } from "@/context/ThemeContext";
 import { useEffect, useState } from "react";
-
 export const Header = () => {
   const pathname = usePathname();
   const { theme, toggleTheme } = useTheme();
@@ -14,11 +13,11 @@ export const Header = () => {
   }, []);
 
   return (
-    <header className='w-full py-4 px-8 flex justify-between items-center bg-primary-bg dark:bg-dark-primary-bg text-primary-text dark:text-dark-primary-text'>
-      <nav className='flex gap-8'>
+    <header className='w-full py-4 px-8 flex md:grid md:grid-cols-3 items-center bg-primary-bg dark:bg-dark-primary-bg text-primary-text dark:text-dark-primary-text'>
+      <nav className='flex gap-8 md:gap-20 md:col-start-2 md:justify-center'>
         <Link
           href='/'
-          className={`font-heading hover:opacity-70 transition-opacity ${
+          className={`font-heading hover:opacity-70 transition-all text-xl md:text-4xl hover:-translate-y-0.5 ${
             pathname === "/" ? "underline underline-offset-8" : ""
           }`}
         >
@@ -26,7 +25,7 @@ export const Header = () => {
         </Link>
         <Link
           href='/cv'
-          className={`font-heading hover:opacity-70 transition-opacity ${
+          className={`font-heading hover:opacity-70 transition-all text-xl md:text-4xl hover:-translate-y-0.5 ${
             pathname === "/cv" ? "underline underline-offset-8" : ""
           }`}
         >
@@ -34,20 +33,20 @@ export const Header = () => {
         </Link>
         <Link
           href='/lab'
-          className={`font-heading hover:opacity-70 transition-opacity ${
+          className={`font-heading hover:opacity-70 transition-all text-xl md:text-4xl hover:-translate-y-0.5 ${
             pathname === "/lab" ? "underline underline-offset-8" : ""
           }`}
         >
           Lab
         </Link>
       </nav>
-      <div className='w-8 h-8 flex items-center justify-center'>
+      <div className='h-8 md:h-10 flex items-center justify-end md:col-start-3 md:justify-end w-full'>
         {mounted && (
           <button
             onClick={toggleTheme}
-            className='text-xl hover:blur-sm transition-all duration-200 opacity-0 animate-fade-in'
+            className='text-2xl md:text-4xl md:hover:blur-sm transition-all duration-200 opacity-0 animate-fade-in active:blur-md active:opacity-50 active:animate-spin'
           >
-            {theme === "light" ? "☀️" : "🌙"}
+            {theme === "light" ? "🌞" : "🌙"}
           </button>
         )}
       </div>
